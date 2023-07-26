7d98e0da5b9ce5d885d1ec81d68d7645f90f766b
Paula Ferreira <paulaalexandra1903@hotmail.com>
2023-07-26 14:56:25 -0300

    adicionando tela inicial com api

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,26 +1,62 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './style.css';
 import client from '../../client';
 
+import logoPokemon from '../../assets/images/pokemon.png';
+import PokemonCard from '../../components/PokemonCard';
+
+type PokemonProps = {
+  id: string,
+  name: string,
+  weight: number,
+  height: number,
+  base_experience: number
+}
+
 function Main() {
+  const [pokemons, setPokemons] = useState<PokemonProps[]>([]);
+
+  async function loadData(){
+    const { data } = await client.get("pokemon/?limit=60");
+
+    const connections:any = [];
+    const pokemonsTemp:PokemonProps[] = [];
+
+    data.results.forEach((item: any) => {
+      connections.push(client.get(`pokemon/${item.name}`));
+    })
+    const result = await Promise.all(connections);
+    result.forEach(resultItem => pokemonsTemp.push({...resultItem.data}));
+    setPokemons([...pokemonsTemp]);
+  }
 
   useEffect(() => {
-    client.get('/path')
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    loadData();
   }, [])
 
   return (
     <div className="App">
       <header>
-        <p>
-          Página Principal
-        </p>
+        <img src={logoPokemon} alt='logomarca do Pokemon' />
+        <h1>
+          Encontre o seu Pokemon favorito!
+        </h1>
       </header>
+      <div className='cards'>
+        {pokemons.map((pokemon: PokemonProps) => {       
+          return(
+            <PokemonCard
+              key={pokemon?.id}
+              pokemonId={pokemon?.id}
+              pokemonName={pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}
+              pokemonImage={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon?.id.toString().padStart(3, '0')}.png`}
+              pokemonHeight={pokemon?.height}
+              pokemonWeight={pokemon?.weight}
+              pokemonExperience={pokemon?.base_experience}
+            />
+          )
+        })}
+      </div>
     </div>
   );
 }
